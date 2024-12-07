e1ef8aa45944774e66f64667d8640688b952c1ab
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2024-12-07 10:32:00 +0100

    Customized Exception handlers

--- src/main/java/com/softserve/itacademy/todolist/exception/GlobalExceptionHandler.java
+++ src/main/java/com/softserve/itacademy/todolist/exception/GlobalExceptionHandler.java
@@ -7,34 +7,47 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 
 @Slf4j
 @RestControllerAdvice
-public class GlobalExceptionHandler { // consider extending ResponseEntityExceptionHandler
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler { // consider extending ResponseEntityExceptionHandler
 
     @ExceptionHandler
     public ResponseEntity<?> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
-        // todo
+        ex.getBindingResult().getAllErrors()
+                .forEach(error -> log.error(error.getDefaultMessage()));
         return ResponseEntity.badRequest().body(ex.getBindingResult().toString());
     }
 
     @ExceptionHandler
     public ResponseEntity<?> handleNullEntityReferenceException(NullEntityReferenceException ex) {
         // todo
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        log.error("Null entity reference: {}", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
     @ExceptionHandler
     public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException ex) {
         // todo: add logging
-        return ResponseEntity.notFound().build();
+        log.error("Entity not found: {}", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
     // todo: add more handlers
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<?> handleAccessDeniedException(AccessDeniedException ex) {
+        log.warn("Access denied: {}", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGlobalException(Exception ex) {
+        log.error("Unhandled exception occurred: {}", ex.getMessage(), ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+
 }
